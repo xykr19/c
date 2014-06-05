0ba5e3c28939eb7eab66a09398635d82c9b7675b
tch <1072580995@qq.com>
2014-06-05 17:40:47 +0800

    first commit

---
+++ src/func/func04.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+void main()  //stdcall  ×ÔÓÒÏò×ó
+{
+    int f(int a,int b);
+
+    int i=2,p;
+
+	//p=f(i,++i);
+    p=f(i,i++);  
+
+    printf("%d\n",p);  //0
+}
+
+int f(int a,int b) //a=3 b=3
+{
+    int c;
+    if(a>b)
+    {
+        c=1;
+    }
+    else if(a==b) 
+    {
+        c=0;
+    }
+    else
+    {
+        c=-1;
+    }
+    return c;
+}
