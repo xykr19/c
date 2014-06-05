14fdd9de336b6c66054a4b16b7d18bd500b4d6a5
tch <1072580995@qq.com>
2014-06-05 17:35:58 +0800

    first commit

---
+++ src/func/func01.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void main()
+{
+	void printstar();
+	void print_message();
+
+	printstar();
+	print_message();
+	printstar();
+}
+
+void printstar()
+{
+	printf("******************\n");
+}
+
+void print_message()
+{
+	printf("How do you!\n");
+}
